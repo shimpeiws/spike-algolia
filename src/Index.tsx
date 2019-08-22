71ccd511390ac5ad98f257401d84eb36ab057dc5
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-08-22 16:52:43 +0900

    Introduce react-instasearch-dom

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Hello from './components/Hello';
+import { Search } from './components/InstantSearch';
 
-ReactDOM.render(<Hello content="hello world" />, document.getElementById('app'));
+ReactDOM.render(
+  <div>
+    <Hello content="hello world" />
+    <br />
+    <Search indexName="contacts" key="instantSearch" />
+  </div>,
+  document.getElementById('app')
+);
